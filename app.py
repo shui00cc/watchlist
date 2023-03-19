f59fc542fed05118f86a4fd2a68867bfd4946c94
shui00cc <384354037@qq.com>
2023-03-19 15:56:44 +0800

    Add minimal home page

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask,url_for
+from markupsafe import escape
 
 app = Flask(__name__)
+app.run(debug=True)
 
 """
 使用 app.route() 装饰器来为这个函数绑定对应的 URL，当用户在浏览器访问这个 URL 的时候，就会触发这个函数，获取返回值，并把返回值显示到浏览器窗口
 """
 @app.route('/')
 def hello():
-    return 'Welcome to My Watchlist!'
+    return 'Hello'
+
+@app.route('/user/<name>')
+def user_page(name):
+    return f'User:{escape(name)}'
+
+@app.route('/test')
+def test_url_for():
+    print(url_for('hello'))
+    print(url_for('user_page', name='cc'))
+    print(url_for('user_page', name='peter'))
+    print(url_for('test_url_for'))
+    print(url_for('test_url_for', num=2))
+    return '<h1>Test Page</h1>'
