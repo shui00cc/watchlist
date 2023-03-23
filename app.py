a6bf8273cd39ae72d286f98e330fa30898c1ba4d
shui00cc <384354037@qq.com>
2023-03-23 10:55:41 +0800

    Add base template and error template

--- app.py
+++ app.py
@@ -18,17 +18,6 @@ class Movie(db.Model):
     title = db.Column(db.String(60))
     year = db.Column(db.String(4))
 
-
-
-"""
-使用 app.route() 装饰器来为这个函数绑定对应的 URL，当用户在浏览器访问这个 URL 的时候，就会触发这个函数，获取返回值，并把返回值显示到浏览器窗口
-"""
-@app.route('/')
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template('index.html', user=user, movies=movies)
-
 @app.cli.command()
 def forge():
     """Generate fake data."""
@@ -57,3 +46,20 @@ def forge():
 
     db.session.commit()
     click.echo('Done.')
+
+@app.context_processor
+def inject_user():
+    user = User.query.first()
+    return dict(user=user)
+
+@app.errorhandler(404)  # 传入要处理的错误代码
+def page_not_found(e):  # 接受异常对象作为参数
+    return render_template('404.html'), 404  # 返回模板和状态码
+
+"""
+使用 app.route() 装饰器来为这个函数绑定对应的 URL，当用户在浏览器访问这个 URL 的时候，就会触发这个函数，获取返回值，并把返回值显示到浏览器窗口
+"""
+@app.route('/')
+def index():
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template('index.html', movies=movies)
